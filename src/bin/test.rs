fc076d1249ea465a80414b22701764a248702521
laurentiustefan97 <steflaurentiu@gmail.com>
2020-05-11 02:55:26 +0300

    Implemented inline support

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,18 +1,56 @@
 use backtrace::backtrace::BacktraceGenerator;
 
-fn tar() {
+trait MyTrait {
+    fn test(&self);
+}
+
+struct MyStruct { }
+
+impl MyStruct {
+    fn new() -> MyStruct {
+        MyStruct {
+
+        }
+    }
+}
+
+impl MyTrait for MyStruct {
+    fn test(&self) {
+        taz();
+    }
+}
+
+fn tazz() {
     let backtrace_generator = BacktraceGenerator::new();
     backtrace_generator.unwind_stack();
 }
 
+
+#[inline(always)]
+fn taz() {
+    tazz();
+}
+
+#[inline(always)]
+fn tar() {
+    let my_struct = MyStruct::new();
+    my_struct.test();
+}
+
+#[inline(always)]
 fn bar() {
     tar();
 }
 
+#[inline(always)]
 fn foo() {
     bar();
 }
 
 fn main() {
+    let mut v = [1, 2, 3, 4, 5];
+    v[0] = 5;
+    v[1] = 2;
+    v[2] = 3;
     foo();
 }
